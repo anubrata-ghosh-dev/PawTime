6a0d397d34f59b6890054d78f6d54ac7faf1e63c
Anubrata Ghosh <study.anubrataghosh@gmail.com>
2026-05-04 13:53:55 +0530

    Configure environment variables for Firebase and Vercel

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBNst36eILYmWf2Md608qAbxQK2_a84PNI",
-  authDomain: "pawtime-2026.firebaseapp.com",
-  projectId: "pawtime-2026",
-  storageBucket: "pawtime-2026.firebasestorage.app",
-  messagingSenderId: "625361779888",
-  appId: "1:625361779888:web:7daf137d98fffdb2d44f75"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
